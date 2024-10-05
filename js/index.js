afe6193aea8041d552d77196c620ac8cfc9d0074
Abdullah <graphicdesigner.abdullah@gmail.com>
2024-10-05 17:09:07 +0600

    Almost completed

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 // "use strict";
 
+const searchInput = document.getElementById("searchInput");
+document.getElementById("searchBtn").addEventListener("click", () => {
+  loadPhones(searchInput.value);
+});
+
 const loadPhones = async (searchText, dataLimit) => {
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
   const response = await fetch(url);
@@ -10,12 +15,13 @@ const loadPhones = async (searchText, dataLimit) => {
 const displayPhones = (items, dataLimit) => {
   const showAll = document.getElementById("showAll");
 
-  if (dataLimit && items.length >= 10) {
-    items = items.slice(0, 10);
-    showAll.classList.remove("hidden");
-  } else showAll.classList.add("hidden");
+  if (dataLimit && items.length >= 9) {
+    items = items.slice(0, 9);
+    // showAll.classList.remove("hidden");
+  } // else showAll.classList.add("hidden");
 
   const allItems = document.getElementById("allItems");
+  allItems.innerHTML = "";
   items.forEach((item) => {
     const { brand, phone_name, slug, image } = item;
     const div = document.createElement("div");
@@ -32,6 +38,7 @@ const displayPhones = (items, dataLimit) => {
             </p>
             <p class="text-xl font-bold">$999</p>
             <button
+            onclick="productDetails('${slug}')"
             class="btn border-none hover:border-none px-10 bg-blue-500 text-white hover:bg-blue-600"
             >
             Show Details
@@ -42,8 +49,48 @@ const displayPhones = (items, dataLimit) => {
   });
 };
 
-document.getElementById("showAll").addEventListener("click", () => {
-  loadPhones("apple");
+let turn = true;
+const showAll = document.getElementById("showAll");
+showAll.addEventListener("click", () => {
+  if (turn) {
+    loadPhones("apple");
+    showAll.innerText = "Show Less";
+    turn = false;
+  } else {
+    loadPhones("apple", 10);
+    showAll.innerText = "Show All";
+    turn = true;
+  }
 });
+
+// Show product details
+
+const loadDetails = async (id) => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${id}`
+  );
+  const data = await response.json();
+
+  makeDetails(data.data);
+};
+
+const makeDetails = (item) => {
+  document.getElementById("modalImg").setAttribute("src", item.image);
+  document.getElementById("modalTitle").innerText = item.name;
+  document.getElementById("Storage").innerText = item.mainFeatures.storage;
+  document.getElementById("Display").innerText = item.mainFeatures.displaySize;
+  document.getElementById("Chipset").innerText = item.mainFeatures.chipSet;
+  document.getElementById("Memory").innerText = item.mainFeatures.memory;
+  document.getElementById("Slug").innerText = item.slug;
+  document.getElementById("ReleaseDate").innerText = item.releaseDate;
+  document.getElementById("Brand").innerText = item.brand;
+  document.getElementById("GPS").innerText = item.others.GPS;
+
+  my_modal_5.showModal();
+};
+
+const productDetails = (id) => {
+  loadDetails(id);
+};
 
 loadPhones("apple", 10);
